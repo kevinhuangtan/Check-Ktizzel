c8674b2c908f9a8ea902630b061d0c4318c025fc
Paul Styslinger <pablitod123@gmail.com>
2015-04-09 01:18:40 -0400

    merged css, graphs, and meteor app

--- checkApp/client/main.js
+++ checkApp/client/main.js
@@ -1,7 +1,83 @@
 Router.map(function() {
-	this.route("home", {path: '/'}) ;
+	this.route("home", {path: '/'});
 });
 
+Router.route("viewEvent", {path: '/viewEvent'});
+
+Template.home.helpers({
+	'user': function(){
+		return Meteor.users.find().fetch()
+	}
+})
 
 // Meteor.user().services.facebook.name
 // Geolocation.currentLocation().coords.latitude
+
+
+//c3.js charts code
+//------------------------------------------------------------//
+
+var donutchart = c3.generate({
+            bindto: '#ethnicity_donut_chart',
+            size: {
+                height: 240,
+                width: 480
+            },
+            data: {
+                columns: [
+                    ['Loading, please standby...', 30],
+                ],
+                type : 'donut',
+                onclick: function (d, i) { console.log("onclick", d, i); },
+                onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+                onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+            },
+            donut: {
+                title: "Ethnicity"
+            }
+        });
+        //in final code we can do this without timeout but keeping in case we need to know how
+        setTimeout(function () {
+            donutchart.load({
+                columns: [
+                    ["White", 0.2, 0.2, 0.2, 0.2, 0.2, 0.4, 0.3, 0.2, 0.2, 0.1, 0.2, 0.2, 0.1, 0.1, 0.2, 0.4, 0.4, 0.3, 0.3, 0.3, 0.2, 0.4, 0.2, 0.5, 0.2, 0.2, 0.4, 0.2, 0.2, 0.2, 0.2, 0.4, 0.1, 0.2, 0.2, 0.2, 0.2, 0.1, 0.2, 0.2, 0.3, 0.3, 0.2, 0.6, 0.4, 0.3, 0.2, 0.2, 0.2, 0.2],
+                    ["African-American", 1.4, 1.5, 1.5, 1.3, 1.5, 1.3, 1.6, 1.0, 1.3, 1.4, 1.0, 1.5, 1.0, 1.4, 1.3, 1.4, 1.5, 1.0, 1.5, 1.1, 1.8, 1.3, 1.5, 1.2, 1.3, 1.4, 1.4, 1.7, 1.5, 1.0, 1.1, 1.0, 1.2, 1.6, 1.5, 1.6, 1.5, 1.3, 1.3, 1.3, 1.2, 1.4, 1.2, 1.0, 1.3, 1.2, 1.3, 1.3, 1.1, 1.3],
+                    ["Hispanic", 2.5, 1.9, 2.1, 1.8, 2.2, 2.1, 1.7, 1.8, 1.8, 2.5, 2.0, 1.9, 2.1, 2.0, 2.4, 2.3, 1.8, 2.2, 2.3, 1.5, 2.3, 2.0, 2.0, 1.8, 2.1, 1.8, 1.8, 1.8, 2.1, 1.6, 1.9, 2.0, 2.2, 1.5, 1.4, 2.3, 2.4, 1.8, 1.8, 2.1, 2.4, 2.3, 1.9, 2.3, 2.5, 2.3, 1.9, 2.0, 2.3, 1.8],
+                    ["Asian", 2.5, 1.9, 2.1, 1.8, 2.2, 2.1, 1.7, 1.8, 1.8, 2.5, 2.0, 1.9, 2.1, 2.0, 2.4, 2.3, 1.8, 2.2, 2.3, 1.5, 2.3, 2.0, 2.0, 1.8, 2.1, 1.8, 1.8, 1.8, 2.1, 1.6, 1.9, 2.0, 2.2, 1.5, 1.4, 2.3, 2.4, 1.8, 1.8, 2.1, 2.4, 2.3, 1.9, 2.3, 2.5, 2.3, 1.9, 2.0, 2.3, 1.8],
+                    ["Other", 2.5, 1.9, 2.1, 1.8, 2.2, 2.1, 1.7, 1.8, 1.8, 2.5, 2.0, 1.9, 2.1, 2.0, 2.4, 2.3, 1.8, 2.2, 2.3, 1.5, 2.3, 2.0, 2.0, 1.8, 2.1, 1.8, 1.8, 1.8, 2.1, 1.6, 1.9, 2.0, 2.2, 1.5, 1.4, 2.3, 2.4, 1.8, 1.8, 2.1, 2.4, 2.3, 1.9, 2.3, 2.5, 2.3, 1.9, 2.0, 2.3, 1.8],
+                ]
+            });
+        }, 1500);
+
+        setTimeout(function () {
+            donutchart.unload({
+                ids: 'Loading, please standby...'
+            });
+        }, 2500);
+
+    var attendencelinechart = c3.generate({
+            bindto: '#attendence_rate_line',
+            size: {
+                height: 240,
+                width: 480
+            },
+            data: {
+                x: 'x',
+                columns: [
+                    ['x','2015-01-01', '2015-01-02', '2015-01-03', '2015-01-04', '2015-01-05', '2015-01-06'],
+                    ['Overall attendence', 0.8, 0.9, 0.85, 0.7, 0.65, 0.7],
+                ]
+            },
+            axis: {
+                x: {
+                    type: 'timeseries',
+                    tick: {
+                        format: '%Y-%m-%d'
+                    }
+                }
+            }
+        });
+//------------------------------------------------------------//
+
+
+
