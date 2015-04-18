9c6b1dd8a451c80ecdb4cf5db833dfc550d28bb9
Albert Shin <albertshin@Alberts-MacBook-Air-344.local>
2015-04-18 16:19:16 -0400

    reverted

--- server/router.js
+++
@@ -1,8 +0,0 @@
-// Router.map(function() {
-// 	this.route("home", {path: '/'});
-// });
-
-// Router.route("viewEvent", {path: '/viewEvent'});
-// Router.route("createEvent", {path: '/create'});
-
-    // return checkEvents.find({"location": { $geoWithin : { $center : [ [-74, 40.74 ] , 10 ] } } } );
