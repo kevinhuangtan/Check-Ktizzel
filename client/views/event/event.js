7b4e05c67c92200ab2e79bc17f32ac6ea2fc895a
Kevin Tan <kevin.tan@yale.edu>
2015-06-10 00:55:25 -0400

    event styling

--- client/views/event/event.js
+++ client/views/event/event.js
@@ -31,6 +31,27 @@ Template.event.helpers({
 		else{
 			return false
 		}
+	},
+	address : function(){
+		var lat = Session.get('eventLocation').lat
+		var lng = Session.get('eventLocation').lng
+		var js;
+		var url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+lng+'&key=AIzaSyA3IaCGmNfltsyk5fyATz-icw-D5VFhSYw';
+		var cityName0;
+		var cityName;
+		$.ajaxSetup({
+		   async: false
+		});
+		$.getJSON(url, function(data) {
+				js = data;
+				cityName0 = js.results[0].address_components[0].long_name;
+				cityName01 = js.results[0].address_components[1].long_name;
+				cityName = js.results[0].address_components[2].long_name;
+				cityName1 = js.results[0].address_components[3].long_name;
+
+		});
+		var result = cityName0 + ' '+cityName01+', '+cityName + ', ' + cityName1;
+		return result
 	}
 });
 
@@ -39,6 +60,13 @@ Template.event.events({
 	'click .checkIn':function(){
 		var eventId = Session.get("currentEvent")
 		checkEvents.update({_id: eventId}, {$push: {attending: Meteor.userId()}});
+	},
+	'click .remove-button':function(){
+		checkEvents.remove(Session.get("currentEvent"));
+		Router.go('splash')
+	},
+	'click .learn-more' : function(){
+
 	}
 })
 
@@ -46,13 +74,16 @@ Template.event.onRendered(function(){
 	var result = checkEvents.findOne({_id: Session.get("currentEvent")})
 	var map;
     var geoLocation = result.geoLocation || { lat: 40.7, lng: -74 };
-        var mapCanvas = document.getElementById('map-canvas');
-        var mapOptions = {
-          center: new google.maps.LatLng(geoLocation.lat,geoLocation.lng),
-          zoom: 16,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
-        map = new google.maps.Map(mapCanvas, mapOptions)
+    Session.set('eventLocation', geoLocation);
+    var mapCanvas = document.getElementById('map-canvas');
+   
+    var mapOptions = {
+      center: new google.maps.LatLng(geoLocation.lat,geoLocation.lng),
+      zoom: 14,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      disableDefaultUI: true
+    }
+    map = new google.maps.Map(mapCanvas, mapOptions)
 	var marker = new google.maps.Marker({
 	  position: geoLocation,
 	  map: map,
