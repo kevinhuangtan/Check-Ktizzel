0feb4c46357f054210c77b3abab3967eb15befc7
Robert Tung <gnuttrebor07@gmail.com>
2015-06-19 00:52:05 -0500

    added autocomplete to createEvent

--- client/views/event/createEvent.js
+++ client/views/event/createEvent.js
@@ -1,3 +1,10 @@
+/*GoogleMaps.init({
+  'sensor': false, //optional
+  'key': 'AIzaSyA3IaCGmNfltsyk5fyATz-icw-D5VFhSYw',
+  'language': 'en',  //optional
+  'libraries': 'geometry,places'
+});*/
+
 // CONSTANTS////////
 var WEEKDAY = new Array(7);
 WEEKDAY[0]=  "SUN";
@@ -217,11 +224,72 @@ Template.eventLocation.events({
 //     }
 //     initialize();
 // });
+/*
+var map;
+var infowindow;
+
+function initialize() {
+  var currentCenter = new google.maps.LatLng(geoLocation.lat,geoLocation.lng);
+
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
+    center: currentCenter,
+    zoom: 15
+  });
+
+  var request = {
+    location: currentCenter,
+    radius: 500,
+    types: ['establishment']
+  };
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);*/
 
 if (Meteor.isClient) {
-  Meteor.startup(function() {
-    GoogleMaps.load();
-  });
+	Meteor.startup(function() {
+		GoogleMaps.load({v:'3', key:'AIzaSyA3IaCGmNfltsyk5fyATz-icw-D5VFhSYw', libraries:'places'});
+	});
+	window.onload = function() {
+		var autocomplete = new google.maps.places.Autocomplete(
+		  (document.getElementById('autocomplete')),{types: ['geocode'] }
+		);
+	};
+/*
+  var defaultBounds = new google.maps.LatLngBounds(
+    new google.maps.LatLng(geoLocation.lat-0.5,geoLocation.lng-0.5),
+    new google.maps.LatLng(geoLocation.lat+0.5,geoLocation.lng+0.5));
+
+  var input = document.getElementById('autocomplete');
+  var options = {
+    bounds: defaultBounds,
+    types: ['establishment']
+  };
+
+  autocomplete = new google.maps.places.Autocomplete(input, options);*/
 
   Template.eventLocation.helpers({
     exampleMapOptions: function() {
